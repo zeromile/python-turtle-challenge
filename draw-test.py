38978310d34bd17019c30520781cb58f4b1e3a38
zeromile <jasonleecooksey@gmail.com>
2019-05-17 12:26:28 -0700

    Create draw-test.py

---
+++ draw-test.py
@@ -0,0 +1,19 @@
+import turtle
+
+# Input your moves
+moves = input("Create a sequence using the letters 'w', 'a', and 's' to control the movement of the turtle. Press enter when you are done. ")
+
+# Use a for loop to cycle through the contents of the moves variable.
+# look here for more info https://www.w3schools.com/python/python_for_loops.asp
+
+for in :
+  # use an if to determine if the 'w' key was pressed 
+  # if the 'w' key is pressed then move the turtle forward by 20
+  
+  # use an if to determine if the 'a' key was pressed 
+  # if the 'a' key is pressed then turn the turtl to the left by 15
+  
+  # use an if to determine if the 's' key was pressed 
+  # if the 's' key is pressed then turn the turtle to the right by 15
+
+turtle.exitonclick()
